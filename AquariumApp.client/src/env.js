53407e5d0a81481e3ff2ee295c5594522b78e5e8
hannah hayward <hannahhayward97@gmail.com>
2021-07-10 00:07:57 -0600

    set up

--- AquariumApp.client/src/env.js
+++ AquariumApp.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
+export const domain = 'hsquared.us.auth0.com'
 export const audience = ''
-export const clientId = ''
+export const clientId = '41N7PBPbeefT1tkZu2oPdBgVMWrVxISj'
